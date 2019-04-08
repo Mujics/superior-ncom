5f5f46062a022419dfb0536a5b5f8a8131cf99ff
Lucas Mujica <lucas.mujica@wolox.com.ar>
2019-04-08 20:45:50 -0300

    Initial Commit

---
+++ src/main/java/superior/ncom/NcomApplication.java
@@ -0,0 +1,13 @@
+package superior.ncom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcomApplication.class, args);
+	}
+
+}
